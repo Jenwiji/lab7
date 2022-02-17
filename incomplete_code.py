8091be12dd863e937b294b4f0fd37b4571d18c21
Jenwiji <63769772+Jenwiji@users.noreply.github.com>
2022-02-17 17:28:52 +0700

    Updated incomplete_code.py

--- incomplete_code.py
+++ incomplete_code.py
@@ -1,37 +1,53 @@
-class Transportation(object):
-   """Abstract base class"""
+from abc import ABC,abstractmethod
+class Transportation(ABC):
 
-   def __init__( self, start, end, distance ):
-      if self.__class__ == Transportation:
-         raise NotImplementedError
-      self.start = start
-      self.end = end
-      self.distance = distance
+    def __init__(self, start_place, end_place, distance):
+        self.start = start_place
+        self.end = end_place
+        self.distance = distance
+        
+    @abstractmethod
+    def find_cost(self):
+        pass
 
-   def find_cost( self ):
-      """Abstract method; derived classes must override"""
-      raise NotImplementedError
+class Walk(Transportation):
 
+    def __init__(self, start_place, end_place, distance):
+        super().__init__(start_place, end_place, distance)
 
-class Walk( Transportation ):
+    def find_cost(self):
+        cost = 0
+        return cost
 
-   def __init__( self, start, end, distance ):
-      Transportation.__init__( self, start, end, distance)
+class Taxi(Transportation):
 
-   def find_cost( self ):
-      return 0
+    def __init__(self, start_place, end_place, distance):
+        super().__init__(start_place, end_place, distance)
 
+    def find_cost(self):
+        price = 40
+        cost = self.distance * price
+        return cost
 
-   
-# main program
+class Train(Transportation):
 
-travel_cost = 0
+    def __init__(self,start_place, end_place, distance, station):
+        super().__init__(start_place, end_place, distance)
+        self.station = station
 
-#trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
-         #Taxi("KMITL SCB Bank","Ladkrabang Station",5),
-         #Train("Ladkrabang Station","Payathai Station",40,6),
-         #Taxi("Payathai Station","The British Council",3) ]
+    def find_cost(self):
+        price = 5
+        cost = self.station * price
+        return cost
 
-for travel in trip:
-   travel_cost += travel.find_cost()
-print travel_cost
+w = Walk("KMITL", "KMITL SCB Bank", 0.6)
+print(w.find_cost())
+
+ta1 = Taxi("KMITL SCB Bank", "Ladkrabang Station", 5)
+print(ta1.find_cost())
+
+tr = Train("Ladkrabang Station", "Payathai Station", 40, 6)
+print(tr.find_cost())
+
+ta2 = Taxi("Payathai Station", "The British Council", 3)
+print(ta2.find_cost())
